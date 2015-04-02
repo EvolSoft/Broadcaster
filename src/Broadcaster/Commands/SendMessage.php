8d70a95107111cf0de5d3620ebd377c9d4107033
Flavius12 <info@evolsoft.tk>
2015-04-02 16:02:29 +0200

    Update SendMessage.php

--- src/Broadcaster/Commands/SendMessage.php
+++ src/Broadcaster/Commands/SendMessage.php
@@ -1,11 +1,11 @@
 <?php
 
 /*
- * Broadcaster (v1.14) by EvolSoft
+ * Broadcaster (v1.15) by EvolSoft
  * Developer: EvolSoft (Flavius12)
  * Website: http://www.evolsoft.tk
  * Date: 27/12/2014 01:48 PM (UTC)
- * Copyright & License: (C) 2014 EvolSoft
+ * Copyright & License: (C) 2014-2015 EvolSoft
  * Licensed under MIT (https://github.com/EvolSoft/Broadcaster/blob/master/LICENSE)
  */
 
