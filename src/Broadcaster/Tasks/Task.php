75eb5cf4edb5de4f82850448899c3176954b2715
Flavius12 <info@evolsoft.tk>
2015-05-28 17:28:36 +0200

    Update and rename src/Broadcaster/Task.php to src/Broadcaster/Tasks/Task.php

--- src/Broadcaster/Tasks/Task.php
+++ src/Broadcaster/Tasks/Task.php
@@ -1,21 +1,23 @@
 <?php
 
 /*
- * Broadcaster (v1.15) by EvolSoft
+ * Broadcaster (v1.16) by EvolSoft
  * Developer: EvolSoft (Flavius12)
  * Website: http://www.evolsoft.tk
- * Date: 27/12/2014 01:48 PM (UTC)
+ * Date: 28/05/2015 01:31 PM (UTC)
  * Copyright & License: (C) 2014-2015 EvolSoft
  * Licensed under MIT (https://github.com/EvolSoft/Broadcaster/blob/master/LICENSE)
  */
 
-namespace Broadcaster;
+namespace Broadcaster\Tasks;
 
 use pocketmine\Server;
 use pocketmine\scheduler\PluginTask;
 use pocketmine\utils\TextFormat;
 
-class Task extends PluginTask{
+use Broadcaster\Main;
+
+class Task extends PluginTask {
 
     public function __construct(Main $plugin){
         parent::__construct($plugin);
@@ -25,14 +27,14 @@ class Task extends PluginTask{
 
     public function onRun($currentTick){
     	$this->plugin = $this->getOwner();
-    	$this->plugin->cfg = $this->plugin->getConfig()->getAll();
-    	if($this->plugin->cfg["broadcast-enabled"]==true){
+    	$this->cfg = $this->plugin->getConfig()->getAll();
+    	if($this->cfg["broadcast-enabled"]==true){
     		$this->length=$this->length+1;
-    		$messages = $this->plugin->cfg["messages"];
+    		$messages = $this->cfg["messages"];
     		$messagekey = $this->length;
     		$message = $messages[$messagekey];
     		if($this->length==count($messages)-1) $this->length = -1;
-    		Server::getInstance()->broadcastMessage($this->plugin->translateColors("&", $this->plugin->broadcast($this->plugin->cfg, $message)));
+    		Server::getInstance()->broadcastMessage($this->plugin->translateColors("&", $this->plugin->broadcast($this->cfg, $message)));
     	}
     }
 
